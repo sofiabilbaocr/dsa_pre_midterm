54bb9a080a3ea117bf8f37314f08349991be9b18
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-03-18 10:38:10 -0600

    demo created

---
+++ playlist_demo.py
@@ -0,0 +1,11 @@
+from ll_double_linked import LinkedList
+
+def ejecutar_demo():
+    # 1. La playlist empieza totalmente vacía
+    mi_playlist = LinkedList()
+    print("Playlist creada exitosamente")
+
+
+
+if __name__ == "__main__":
+    ejecutar_demo()
